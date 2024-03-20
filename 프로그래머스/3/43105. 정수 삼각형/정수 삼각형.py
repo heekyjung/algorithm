6a945bc09036fa4d20e6c50041fdad2cd1ed2380
Heeky <69945698+heekyjung@users.noreply.github.com>
2024-03-20 15:47:35 +0900

    [level 3] Title: 정수 삼각형, Time: 81.40 ms, Memory: 18.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,20 @@
+from collections import deque
+def solution(triangle):
+    height = len(triangle)    
+    answers = [[0 for _ in range(len(tri))] for tri in triangle]
+    
+    for i in range(height):
+        for j in range(len(triangle[i])):
+            before = 0
+            if i - 1 >= 0:
+                if j == 0:
+                    before = answers[i-1][j]
+                elif 0 < j < len(triangle[i])-1:
+                    before = max(answers[i-1][j-1], answers[i-1][j])
+                elif j == len(triangle[i]) - 1:
+                    before = answers[i-1][j-1]
+            sum = triangle[i][j] + before
+            answers[i][j] = sum
+
+    # [[7], [10, 15], [18, 16, 15], [20, 25, 20, 19], [24, 30, 27, 26, 24]]
+    return max(answers[-1])
