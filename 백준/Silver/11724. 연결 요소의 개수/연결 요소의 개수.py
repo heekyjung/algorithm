9702912082c314c6be980cc0d4d88ce648c04d78
Heeky <69945698+heekyjung@users.noreply.github.com>
2024-03-22 00:16:55 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 692 ms, Memory: 65228 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,5 +1,6 @@
 import sys
-sys.setrecursionlimit(10**7)
+from collections import deque
+# sys.setrecursionlimit(10**7)
 # sys.stdin = open("sample_input.txt", "r")
 
 # 정점 개수 n, 간선 개수 m
@@ -16,20 +17,36 @@ for _ in range(m):
 
 count = 0
 
-
-def dfs(node):
-    if visited[node]:  # 탈출 조건
-        return
-
+def bfs(node):
+    queue = deque([node])
     visited[node] = True
-    for n in graph[node]:
-        if visited[n] == False:
-            dfs(n)
-
+    
+    while queue:
+        now = queue.popleft()
+        
+        for n in graph[now]:
+            if not visited[n]: # False라면
+                queue.append(n)
+                visited[n] = True
 
 for i in range(1, n+1):
-    if visited[i] == False:
-        dfs(i)
+    if not visited[i]:
+        bfs(i)
         count += 1
+
+# def dfs(node):
+#     if visited[node]:  # 탈출 조건
+#         return
+
+#     visited[node] = True
+#     for n in graph[node]:
+#         if visited[n] == False:
+#             dfs(n)
+
+
+# for i in range(1, n+1):
+#     if visited[i] == False:
+#         dfs(i)
+#         count += 1
 
 print(count)
