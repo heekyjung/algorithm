4e95ba047cb5fe440f677af840bd3eb429ffc478
Heeky <69945698+heekyjung@users.noreply.github.com>
2024-03-29 12:52:35 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 25.77 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -0,0 +1,6 @@
+def solution(arr, queries):
+    for q in queries:
+        for i in range(len(arr)):
+            if q[0] <= i <= q[1] and i % q[2] == 0:
+                arr[i] += 1
+    return arr
